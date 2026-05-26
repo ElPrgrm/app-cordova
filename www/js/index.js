68ea55b91b4c59a8aa9c7bfca559bfcd9478f6c9
Leobardo Daniel Vargas <ldvr30345@gmail.com>
2026-05-26 00:03:07 -0500

    Actualizar enlaces de navegación a la página de ventas y redirigir al cargar la aplicación.

--- www/js/index.js
+++ www/js/index.js
@@ -23,7 +23,7 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+        window.location.href =`ventas.html`;
+/*    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');*/
 }
