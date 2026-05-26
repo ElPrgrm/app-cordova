17b61c2cb846093a12b115bc59c39f98de950ea2
Jona <jona.ramirez.guzman.1784@gmail.com>
2026-05-25 23:27:25 -0500

    ola

--- www/js/escaner.js
+++ www/js/escaner.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1/DDI/API/productos.php';
+const API_BASE_URL = 'http://127.0.0.1/PuntoVenta/www/api/productos.php';
 let formInitialized = false;
 let currentProductId = null;
 
