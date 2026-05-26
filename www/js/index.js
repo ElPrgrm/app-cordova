4ea408ff918ec569417731c123a364935697bc41
Jona <jona.ramirez.guzman.1784@gmail.com>
2026-05-26 14:02:10 -0500

    ayuda

--- www/js/index.js
+++ www/js/index.js
@@ -23,7 +23,7 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-        window.location.href =`index.html`;
+        window.location.href =`ventas.html`;
 /*    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');*/
 }
