a1f7ac56455780f057ecc1a9bf5982dd01ee2d4b
ReneT05 <trevi0527rene@gmail.com>
2026-06-19 23:25:44 -0500

    Cambio

--- www/api/usuarios.php
+++ www/api/usuarios.php
@@ -21,7 +21,7 @@ function generateUuidV4()
 
 $input = json_decode(file_get_contents('php://input'), true) ?: $_POST;
 $action = strtolower(trim($input['action'] ?? 'login'));
-$username = trim($input['name'] ?? '');
+$username = trim($input['username'] ?? '');
 $password = trim($input['password'] ?? '');
 $uuid = trim($input['uuid'] ?? '');
 
